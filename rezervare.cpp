001ee9a20db1bff62887cbad5998b8bdc830087f
laurentiulip <94955316+laurentiulip@users.noreply.github.com>
2025-03-28 08:34:00 +0200

    LL4.cpp

--- rezervare.cpp
+++ rezervare.cpp
@@ -5,18 +5,26 @@
 
 using namespace std;
 
-class Rezervare {
+class IRezervare {
+public:
+    virtual int calculeazaDurata() const = 0;
+    virtual int calculeazaPret() const = 0;
+    virtual string getTip() const = 0;
+    virtual ~IRezervare() {}
+};
+
+class Rezervare : public IRezervare {
 protected:
     string numeClient;
     string dataOra;
     int nrPersoane;
 
 public:
-    void setNumeClient(string nume) {
+    void setNumeClient(const string& nume) {
         this->numeClient = nume;
     }
 
-    void setDataOra(string data) {
+    void setDataOra(const string& data) {
         this->dataOra = data;
     }
 
@@ -24,76 +32,89 @@ public:
         this->nrPersoane = nr;
     }
 
-    virtual int calculeazaDurata() {
-        return 2; // Durata standard a unei mese este 2 ore
+    virtual int calculeazaDurata() const override {
+        return 2;
     }
 
-    virtual int calculeazaPret() {
+    virtual int calculeazaPret() const override {
         return 0;
     }
 
-    virtual string getTip() { return "Rezervare"; }
+    virtual string getTip() const override { 
+        return "Rezervare"; 
+    }
 
-    void afisareInfo() {
+    virtual ~Rezervare() {}
+
+    void afisareInfo() const {
+        
         cout << "Tip: " << getTip() << endl;
         cout << "Client: " << numeClient << endl;
         cout << "Data si ora: " << dataOra << endl;
         cout << "Numar persoane: " << nrPersoane << endl;
         cout << "Durata: " << calculeazaDurata() << " ore" << endl;
         cout << "Pret total: " << calculeazaPret() << " lei" << endl;
-        cout << endl;
+        cout << "------------------------------" << endl;
     }
 };
 
 class RezervareZilnica : public Rezervare {
 public:
-    int calculeazaPret() override {
+    int calculeazaPret() const override {
         return nrPersoane <= 2 ? 50 : 50 + (nrPersoane - 2) * 20;
     }
 
-    string getTip() override { return "Rezervare Zilnica"; }
+    string getTip() const override { 
+        return "Rezervare Zilnica"; 
+    }
 };
 
 class RezervareEvenimentSpecial : public Rezervare {
 private:
-    int oreSuplimentare;
+    int oreSuplimentare = 0;
 
 public:
     void setOreSuplimentare(int ore) {
         this->oreSuplimentare = ore;
     }
 
-    int calculeazaDurata() override {
+    int calculeazaDurata() const override {
         return 4 + oreSuplimentare;
     }
 
-    int calculeazaPret() override {
+    int calculeazaPret() const override {
         return 200 + oreSuplimentare * 50;
     }
 
-    string getTip() override { return "Rezervare Eveniment Special"; }
+    string getTip() const override { 
+        return "Rezervare Eveniment Special"; 
+    }
 };
 
 class RezervareGrupMare : public Rezervare {
 public:
-    int calculeazaPret() override {
+    int calculeazaPret() const override {
         return nrPersoane <= 20 ? 500 : 500 + (nrPersoane - 20) * 10;
     }
 
-    string getTip() override { return "Rezervare Grup Mare"; }
+    string getTip() const override { 
+        return "Rezervare Grup Mare"; 
+    }
 };
 
 class RezervareVIP : public Rezervare {
 public:
-    int calculeazaPret() override {
+    int calculeazaPret() const override {
         return nrPersoane <= 5 ? 1000 : 1000 + (nrPersoane - 5) * 50;
     }
 
-    string getTip() override { return "Rezervare VIP"; }
+    string getTip() const override { 
+        return "Rezervare VIP"; 
+    }
 };
 
 int main() {
-    string path = "rezervari.txt";
+    const string path = "rezervari.txt";
     ifstream fin(path);
     if (!fin.is_open()) {
         cout << "Eroare la deschiderea fisierului " << path << " !" << endl;
@@ -102,7 +123,7 @@ int main() {
 
     vector<Rezervare*> rezervari;
     string tipRezervare, numeClient, dataOra;
-    int nrPersoane, oreSuplimentare;
+    int nrPersoane;
 
     while (fin >> tipRezervare >> numeClient >> dataOra >> nrPersoane) {
         Rezervare* rezervare = nullptr;
@@ -110,28 +131,37 @@ int main() {
         if (tipRezervare == "RezervareZilnica") {
             rezervare = new RezervareZilnica();
         } else if (tipRezervare == "RezervareEvenimentSpecial") {
-            fin >> oreSuplimentare;
-            rezervare = new RezervareEvenimentSpecial();
-            dynamic_cast<RezervareEvenimentSpecial*>(rezervare)->setOreSuplimentare(oreSuplimentare);
+            int oreSuplimentare;
+            if (!(fin >> oreSuplimentare)) {
+                cout << "Date insuficiente pentru RezervareEvenimentSpecial!" << endl;
+                break;
+            }
+            RezervareEvenimentSpecial* rezEveniment = new RezervareEvenimentSpecial();
+            rezEveniment->setOreSuplimentare(oreSuplimentare);
+            rezervare = rezEveniment;
         } else if (tipRezervare == "RezervareGrupMare") {
             rezervare = new RezervareGrupMare();
         } else if (tipRezervare == "RezervareVIP") {
             rezervare = new RezervareVIP();
+        } else {
+            cout << "Tip rezervare necunoscut: " << tipRezervare << endl;
+            continue;
         }
 
-        if (rezervare) {
-            rezervare->setNumeClient(numeClient);
-            rezervare->setDataOra(dataOra);
-            rezervare->setNrPersoane(nrPersoane);
-            rezervari.push_back(rezervare);
-        }
+        rezervare->setNumeClient(numeClient);
+        rezervare->setDataOra(dataOra);
+        rezervare->setNrPersoane(nrPersoane);
+        rezervari.push_back(rezervare);
     }
+
+    fin.close();
 
     for (auto r : rezervari) {
         r->afisareInfo();
         delete r;
     }
 
-    fin.close();
     return 0;
 }
+
+
